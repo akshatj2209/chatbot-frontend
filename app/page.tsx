0bd1ac6afd453a4dbec9e9468b2fc834e6b8766c
akshatj2209 <akshatj2209@gmail.com>
2024-09-08 01:48:05 +0530

    feat: Add header component to widget

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatbotWidget from '@/components/ChatbotWidget';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5 font-['Inter']">
       <ChatbotWidget />
     </main>
   );
---
+++ components/ChatbotWidget/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ActionRow from './ActionRow';
+import ChatbotDetails from './ChatbotDetails';
+
+export default function Header() {
+    return (
+        <div className="flex flex-col mt-5 px-4 gap-[18px]">
+            <ActionRow />
+            <ChatbotDetails />
+        </div>
+    )
+}
