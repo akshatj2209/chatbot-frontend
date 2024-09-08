b4e13fa2fa2ecbac4cd1cecb1625782b93945260
akshatj2209 <akshatj2209@gmail.com>
2024-09-08 15:38:39 +0530

    Pre commit hooks

--- app/(home)/components/ChatInterface/index.tsx
+++ app/(home)/components/ChatInterface/index.tsx
@@ -8,29 +8,31 @@ import { useChatContext } from '@/contexts/ChatContext';
 import { cn } from '@/lib/utils';
 
 export default function ChatInterface() {
-    const { isChatOpen, toggleChat, paneLocation, isMaximized } = useChatContext();
+  const { isChatOpen, toggleChat, paneLocation, isMaximized } = useChatContext();
 
-    return (
-        <div
-            className={cn(
-                isMaximized && 'w-[calc(100%-40px)]',
-                !isMaximized && paneLocation === 'left' ? 'md:fixed md:bottom-5 md:left-5 bottom-0 left-0' : 'fixed md:bottom-5 md:right-5 bottom-0 right-0'
-            )}
-        >
-            {isChatOpen ? (
-                <div className="transition-opacity duration-300 ease-in-out opacity-100">
-                    <ChatbotWidget />
-                </div>
-            ) : (
-                <div className="transition-opacity duration-300 ease-in-out opacity-100">
-                    <Button
-                        onClick={toggleChat}
-                        className="bg-violet-600 hover:bg-violet-800 text-white rounded-full p-3 shadow-lg h-12 w-12"
-                    >
-                        <MessageSquare size={24} />
-                    </Button>
-                </div>
-            )}
+  return (
+    <div
+      className={cn(
+        isMaximized && 'w-[calc(100%-40px)]',
+        !isMaximized && paneLocation === 'left'
+          ? 'md:fixed md:bottom-5 md:left-5 bottom-0 left-0'
+          : 'fixed md:bottom-5 md:right-5 bottom-0 right-0'
+      )}
+    >
+      {isChatOpen ? (
+        <div className="transition-opacity duration-300 ease-in-out opacity-100">
+          <ChatbotWidget />
         </div>
-    );
+      ) : (
+        <div className="transition-opacity duration-300 ease-in-out opacity-100">
+          <Button
+            onClick={toggleChat}
+            className="bg-violet-600 hover:bg-violet-800 text-white rounded-full p-3 shadow-lg h-12 w-12"
+          >
+            <MessageSquare size={24} />
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
